b746235f752f3ec87442cd292a1ed841f5b110cd
Rafael M. Lopes <rafael@rafaelme.com>
2013-04-10 18:25:00 -0300

    Added buttons

--- aTransportA/aTransportA.ino
+++ aTransportA/aTransportA.ino
@@ -8,6 +8,12 @@
 RFID rfid(10,5); 
 SdFat sd;
 SFEMP3Shield MP3player;
+const int button1 = 14; // Analog 0
+const int button2 = 15; // Analog 1
+const int button3 = 16; // Analog 2
+int button1State = 0;
+int button2State = 0;
+int button3State = 0;
 
 void setup()
 { 
@@ -16,6 +22,9 @@ void setup()
   rfid.init();
   sd.begin(SD_SEL, SPI_HALF_SPEED);
   MP3player.begin();
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);  
+  pinMode(button3, INPUT);
 }
 
 void loop()
@@ -38,8 +47,33 @@ void loop()
                         Serial.print(" , ");
 			Serial.print(rfid.serNum[4],DEC);
                         Serial.println(" ");
-
-                        MP3player.playTrack(1);
+                        // Button1
+                        button1State = digitalRead(button1);
+                        if (button1State == HIGH) {  
+                          MP3player.playTrack(1);
+                          Serial.println("Botao1 pressionado");
+                        }
+                        else {
+                          Serial.println("Botao1 nao pressionado");
+                        }
+                        // Button2
+                        button2State = digitalRead(button2);
+                        if (button2State == HIGH) {  
+                          MP3player.playTrack(2);
+                          Serial.println("Botao2 pressionado");
+                        }
+                        else {
+                          Serial.println("Botao2 nao pressionado");
+                        }
+                        // Button3
+                        button3State = digitalRead(button3);
+                        if (button3State == HIGH) {  
+                          MP3player.playTrack(1);
+                          Serial.println("Botao3 pressionado");
+                        }
+                        else {
+                          Serial.println("Botao3 nao pressionado");
+                        }
                         delay(10000);
                        
             
