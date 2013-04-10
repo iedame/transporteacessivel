4b298370f41f75506047f04eae659b4772443b3c
Rafael M. Lopes <rafael@rafaelme.com>
2013-04-10 20:45:53 -0300

    Added Piezo, RF

--- aTransportA/aTransportA.ino
+++ aTransportA/aTransportA.ino
@@ -3,6 +3,7 @@
 #include <SdFatUtil.h> 
 #include <SFEMP3Shield.h>
 #include <RFID.h>
+#include <VirtualWire.h>
 
 
 RFID rfid(10,5); 
@@ -11,9 +12,12 @@ SFEMP3Shield MP3player;
 const int button1 = 14; // Analog 0
 const int button2 = 15; // Analog 1
 const int button3 = 16; // Analog 2
+const int knockSensor = A3;
+const int threshold = 1023;
 int button1State = 0;
 int button2State = 0;
 int button3State = 0;
+int sensorReading = 0;
 
 void setup()
 { 
@@ -25,10 +29,27 @@ void setup()
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);  
   pinMode(button3, INPUT);
+  //vw_set_ptt_inverted(true);  // Required by the RF module ---OBS
+  //vw_setup(2000);            // bps connection speed ---OBS
+  //vw_set_tx_pin(18);         // RF pin ---OBS
 }
 
 void loop()
 {
+   //Message to send:
+   //const char *msg = "HELLO WORLD";
+   //vw_send((uint8_t *)msg, strlen(msg));
+   //vw_wait_tx();        // We wait to finish sending the message
+   //delay(200);         // We wait to send the message again      
+   
+   // Piezo
+   //Serial.print(sensorReading);
+   //sensorReading = analogRead(knockSensor);
+   //if (sensorReading >= threshold) {
+   //Serial.println("Knock!");                   
+   //}
+   //delay(1000);
+                        
     if (rfid.isCard()) {
       
           Serial.println("Valid card");
@@ -47,14 +68,16 @@ void loop()
                         Serial.print(" , ");
 			Serial.print(rfid.serNum[4],DEC);
                         Serial.println(" ");
+                        
                         // Button1
-                        button1State = digitalRead(button1);
-                        if (button1State == HIGH) {  
-                          MP3player.playTrack(1);
-                          Serial.println("Botao1 pressionado");
-                        } else {
-                          Serial.println("Botao1 nao pressionado");
-                        }
+                        //button1State = digitalRead(button1);
+                        //if (button1State == HIGH) {  
+                        //  MP3player.playTrack(1);
+                        //  Serial.println("Botao1 pressionado");
+                        //} else {
+                        //  Serial.println("Botao1 nao pressionado");
+                        //}
+                        
                         // Button2
                         //button2State = digitalRead(button2);
                         //if (button2State == HIGH) {  
@@ -64,6 +87,7 @@ void loop()
                         //else {
                         //  Serial.println("Botao2 nao pressionado");
                         //}
+                        
                         // Button3
                         //button3State = digitalRead(button3);
                         //if (button3State == HIGH) {  
@@ -73,7 +97,8 @@ void loop()
                         //else {
                         //  Serial.println("Botao3 nao pressionado");
                         //}
-                        delay(7000);
+                        
+                        delay(1000);
                        
             
           }
