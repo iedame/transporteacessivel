51baf30aaec5e2347ecdcba722d7493e7830f9ba
Rafael M. Lopes <rafael@rafaelme.com>
2013-04-21 19:42:46 -0300

    Code updated

--- aTransportA/aTransportA.ino
+++ aTransportA/aTransportA.ino
@@ -5,19 +5,26 @@
 #include <RFID.h>
 #include <VirtualWire.h>
 
-
-RFID rfid(10,5); 
+RFID rfid(10,5);
 SdFat sd;
 SFEMP3Shield MP3player;
-const int button1 = 14; // Analog 0
-const int button2 = 15; // Analog 1
-const int button3 = 16; // Analog 2
+
+// Buttons pins
+const int button1 = 14;
+const int button2 = 15;
+const int button3 = 16;
+// Piezo
 const int knockSensor = A3;
-const int threshold = 1023;
+const int calibracao = 1023;
+// Buttons
 int button1State = 0;
 int button2State = 0;
 int button3State = 0;
 int sensorReading = 0;
+// Panel
+int option = 0;
+int validUser = 0;
+boolean waiting = false;
 
 void setup()
 { 
@@ -38,18 +45,16 @@ void setup()
 
 void loop()
 {
-   // Piezo
+  
+// Availability Message
    //Serial.print(sensorReading);
    //sensorReading = analogRead(knockSensor);
-   //if (sensorReading >= threshold) {
+   //if (sensorReading >= calibracao) {
    //Serial.println("Knock!");                   
    //}
    //delay(1000);
                         
     if (rfid.isCard()) {
-      
-          Serial.println("Valid card");
-
           if (rfid.readCardSerial()) {
             
                         Serial.println(" ");
@@ -64,21 +69,35 @@ void loop()
                         Serial.print(" , ");
 			Serial.print(rfid.serNum[4],DEC);
                         Serial.println(" ");
-                        
+                        // Start of card verification
+                        if(rfid.serNum[0] == 137) {
+                          Serial.println("Admin User");
+                          validUser = 1;
+                        } else if(rfid.serNum[0] == 43) {
+                          Serial.println("User");
+                          validUser = 1;
+                        }
+                        // End of verification
+                        if (validUser == 1){
+                          
+                        Serial.println("Sistema ativo");
+                        while (option == 0){
+                        // Painel de selecao
                         // Button1
-                        //button1State = digitalRead(button1);
-                        //if (button1State == HIGH) {  
-                        //  MP3player.playTrack(1);
-                        //  Serial.println("Botao1 pressionado");
-                        //} else {
-                        //  Serial.println("Botao1 nao pressionado");
-                        //}
-                        
+                        button1State = digitalRead(button1);
+                        if (button1State == LOW) {  
+                        //MP3player.playTrack(1);
+                          Serial.println("Botao1 pressionado");
+                          option = 1;
+                        } else {
+                          Serial.println("Botao1 nao pressionado");
+                        }
+                        }
                         // Button2
                         //button2State = digitalRead(button2);
-                        //if (button2State == HIGH) {  
+                        //if (button2State == LOW) {  
                         //  MP3player.playTrack(2);
-                        //  Serial.println("Botao2 pressionado");
+                        //Serial.println("Botao2 pressionado");
                         //}
                         //else {
                         //  Serial.println("Botao2 nao pressionado");
@@ -86,29 +105,45 @@ void loop()
                         
                         // Button3
                         //button3State = digitalRead(button3);
-                        //if (button3State == HIGH) {  
+                        //if (button3State == LOW) {  
                         //  MP3player.playTrack(1);
-                        //  Serial.println("Botao3 pressionado");
+                        //Serial.println("Botao3 pressionado");
                         //}
                         //else {
                         //  Serial.println("Botao3 nao pressionado");
                         //}
-                        uint8_t buf[VW_MAX_MESSAGE_LEN];
-                        uint8_t buflen = VW_MAX_MESSAGE_LEN;
-                        if (vw_get_message(buf, &buflen)){
-                        int i;
-                        // Message with proper check    
-                        for (i = 0; i < buflen; i++)
-                        {
-                        Serial.write(buf[i]);  // The received data is stored in the buffer
-                                               // and sent through the serial port to the computer
-                        }
-                        Serial.println();
-                       
                         }
            delay(1000);
     }
     
     rfid.halt();
+    
+// Notification System
 }
+  if (waiting == true){
+    if(option == 1){
+      //Mensagem Opcao 1
+      
+      uint8_t buf[VW_MAX_MESSAGE_LEN];
+      uint8_t buflen = VW_MAX_MESSAGE_LEN;
+      if (vw_get_message(buf, &buflen)){
+      int i;
+      // Message with proper check    
+      for (i = 0; i < buflen; i++){
+       Serial.write(buf[i]);
+          }
+      Serial.println();
+                       
+      }
+    }
+    if(option == 2){
+     //Mensagem Opcao 2
+    }
+    if(option == 3){
+     //Mensagem Opcao 3
+    }
+  }
+  else {
+  option = 0;
+  }
 }
